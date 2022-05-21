4685e198ea31013a8a86084475a762770a021642
Alex Njiru <75260401+alekione@users.noreply.github.com>
2022-05-21 20:01:39 +0300

    Update AnagramGui.java

--- src/default package/AnagramGui.java
+++ src/default package/AnagramGui.java
@@ -15,7 +15,9 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -33,25 +35,26 @@ public class AnagramGui extends Application {
 	VBox vBox, rBox;
 	HBox buttonBox, hbox;
 	int width, height;
-	Label label, label1, label2;
+	Label countLabel, label, label1, label2;
 	ChoiceBox choiceBox;
 	Button newGame, restart, nextGame, exit;
 	TextField text;
-	Tooltip tip, tip1;
+	Tooltip tip;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage arg0) throws Exception {
+		
 		/**
 		 * Create a scene to represent your graphical drawing.
 		 * We will use buttons to initialize the game.
 		 * play button - to play &&
 		 * exit button to close the game.
 		 */
+		
 		Button play = new Button("PLAY");
 		play.setOnAction(e -> {playGame();});
 		Button exit = new Button("EXIT");
@@ -70,9 +73,10 @@ public class AnagramGui extends Application {
 		borderPane.setCenter(box);
 		
 		
-		Scene scene = new Scene(borderPane,400,400);
+		Scene scene = new Scene(borderPane,600,400);
 		arg0.setScene(scene);
 		arg0.setTitle("ANAGRAM GAME");
+		arg0.setResizable(false);
 		arg0.show();
 	}
 
@@ -88,10 +92,12 @@ public class AnagramGui extends Application {
 		 * we also use vbox and hbox classes to do the alignment of the nodes.
 		 */
 		
+		label = new Label("Game Level");
+		label.setFont(new Font(18));
 		String selections[] = {"Choose a level", "Hard", "Medium", "Simple"};
 		choiceBox = new ChoiceBox(FXCollections.observableArrayList(selections));
-		label = new Label();
-		rBox = new VBox(20, choiceBox, label);
+		countLabel = new Label();
+		rBox = new VBox(20, label, choiceBox, countLabel);
 		
 		label1 = new Label("Please fill in the gaps:\n");
 		label1.setFont(new Font(20));
@@ -175,11 +181,15 @@ public class AnagramGui extends Application {
 		tip.setText("Select to play a new game.");
 		newGame.setOnAction(e -> playGame());
 		newGame.setTooltip(tip);
-		tip1 = new Tooltip();
-		tip1.setText("select to go yo the next word.");
-		tip1.setStyle("-fx-font: normal bold 10 Cursive; " + "-fx-base: #AE3522; ");
-		nextGame.setOnAction(e -> update(choice));
-		nextGame.setTooltip(tip1);
+		tip.setText("select to go yo the next word.");
+		nextGame.setOnAction(e -> {
+			borderPane.setCenter(vBox);
+			text.setText("");
+			update(choice);
+			}
+		);
+		nextGame.setTooltip(tip);
+		
 		label1.setText("Please fill in the gaps:\n" + spacedWord);
 		text.setEditable(true);
 		text.setOnKeyPressed(e -> {
@@ -200,23 +210,32 @@ public class AnagramGui extends Application {
 	 */
 		
 	void checkAnswer(String word, String answer) {
-		label.setText("Attempts remaining\n" + count);
+		countLabel.setText("Attempts remaining\n" + count);
 
+		if (answer.equals("")) {
+			text.setStyle("-fx-text-box-border: #B22222; -fx-focus-color: #B22222;");
+		}
+		else {
+			text.setStyle("");
 			if (word.equals(answer)) {
-				Label newLabel = new Label("Cogratulation your answer is correct.");
-				newLabel.setFont(new Font(25));
-				newLabel.setAlignment(Pos.CENTER);
-				borderPane.setCenter(newLabel);
+			Label newLabel = new Label("Cogratulation your answer is correct.");
+			newLabel.setFont(new Font(25));
+			newLabel.setAlignment(Pos.CENTER);
+			borderPane.setCenter(newLabel);
+				
 			}
 			else if (count == 0) {
 				label1.setText("You have been HANGED!!\n" + 
-						"Maximum number of attemps reached..\n" + "The correct answer is" +
+						"Maximum number of attemps reached..\n" + "The correct answer is " +
 						word);
+				text.setEditable(false);
 			}
 			else {
 				count--;
 				label1.setText("Incorrect answer. Please try Again\n" + spacedWord);
-				label.setText("Attempts remaining\n" + count);
-			}	
+				countLabel.setText("Attempts remaining\n" + count);
+				text.setText("");
+			}
+		}
 	}	
 }
